8202bc3e6aac4615f9ef5240ff0052e0b06bcd63
Cheddabob420 <Dntbanmethistime@yahoo.com>
2025-03-16 19:09:28 -0400

    Update run3.sh

--- Termux-Debian-Setup/run3.sh
+++ Termux-Debian-Setup/run3.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-proot-distro login debian --user user --termux-home -- sh run4.sh
+echo "Logging in as user to setup xfce4 and packages."
+sleep 5
+proot-distro login debian --user user --termux-home -- cd Project-MonDeb && sh run4.sh
