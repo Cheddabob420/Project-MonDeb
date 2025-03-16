0263a4751b1c56fdb6b999f25182752cafef0b32
Cheddabob420 <Dntbanmethistime@yahoo.com>
2025-03-16 19:06:48 -0400

    Update run2.sh

--- Termux-Debian-Setup/run2.sh
+++ Termux-Debian-Setup/run2.sh
@@ -11,5 +11,6 @@ apt install sudo neovim -y
 apt update && apt dist-upgrade -y 
 echo "enter your new password when prompted all other info can be left blank if desired."
 adduser $name
-echo "now editing the sudoers file, you will need to add the line: [ $name  ALL(ALL:ALL) ALL ] below the line: [  root  ALL(ALL:ALL) ALL ] afterwards ( esc :wq )
+echo "now editing the sudoers file, you will need to add the line: [ $name  ALL(ALL:ALL) ALL ] below the line: [  root  ALL(ALL:ALL) ALL ] afterwards ( esc :wq )"
+sleep 8
 visudo
