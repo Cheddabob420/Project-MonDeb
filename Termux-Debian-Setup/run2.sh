2a1098c2f26b596608296b4a281ab8c9018f03a7
Cheddabob420 <Dntbanmethistime@yahoo.com>
2025-03-16 20:32:26 -0400

    Update run2.sh

--- Termux-Debian-Setup/run2.sh
+++ Termux-Debian-Setup/run2.sh
@@ -1,15 +1,16 @@
 #!/bin/bash 
-echo "Adding Main User"
+cowsay "Adding Main User"
 sleep 5
 read -e -i "user" -p "Enter primary username: " name
 export name
-echo "Hello $name"
+cowsay "Hello $name"
 sleep 2
 echo "Lets get your account in debian setup."
 sleep 4
 apt install sudo neovim -y
 apt update && apt dist-upgrade -y 
-echo "enter your new password when prompted all other info can be left blank if desired."
+cowsay "enter your new password when prompted all other info can be left blank if desired(Hit Enter until Complete)."
+sleep 5
 adduser $name
 echo "now editing the sudoers file, you will need to add the line: [ $name  ALL(ALL:ALL) ALL ] below the line: [  root  ALL(ALL:ALL) ALL ] afterwards ( esc :wq )"
 sleep 8
