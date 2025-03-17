8217db5a8e163f0ee0074f4456cef3c8be42dc14
Cheddabob420 <Dntbanmethistime@yahoo.com>
2025-03-16 20:00:21 -0400

    Update run4.sh

--- Termux-Debian-Setup/run4.sh
+++ Termux-Debian-Setup/run4.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
-
-sudo apt install neofetch fish gradle clang cmake cowsay wget nodejs curl xfce4 xfce4-goodies dbus-x11 flatpak libfuse2 libxi6 libxrender1 libxtst6 mesa-utils libfontconfig libgtk-3-bin openjdk-17-jdk -y
-
-sudo apt install openjdk-8-jdk
-
+echo "Package Install and DE setup."
+sleep 4
+echo "Pkg Inst"
+sleep 4
+sudo apt install neofetch -y
+sudo apt install fish -y
+sudo apt install gradle -y
+sudo apt install clang -y
+sudo apt install cmake -y
+sudo apt install cowsay -y
+sudo apt install wget -y
+sudo apt install nodejs -y
+sudo apt install curl -y
+sudo apt install xfce4 -y
+sudo apt install xfce4-goodies -y
+sudo apt install dbus-x11 -y
+sudo apt install flatpak -y
+sudo apt install libfuse2 -y
+sudo apt install libxi6 -y
+sudo apt install libxrender1 -y
+sudo apt install libxtst6 -y
+sudo apt install mesa-utils -y
+sudo apt install libfontconfig -y
+sudo apt install libgtk-3-bin -y
+sudo apt install openjdk-17-jdk -y
+wait
+sudo apt install openjdk-8-jdk -y
+wait
+neofetch
+sleep 10
+cowsay "DE Setup/Config"
 update-alternatives && config x-terminal-emulator
-
 bash -c  "$(curl -fsSL https://raw.githubusercontent.com/officialrajdeepsingh/nerd-fonts-installer/main/install.sh)"
-sudo apt install xfce4-whiskermenu-plugin
-sudo apt install mugshotapt
+sudo apt install xfce4-whiskermenu-plugin -y
+sudo apt install mugshotapt -y
+wait
 apt search gtk-themes
-sudo apt install greybird-gtk-theme
-sudo apt install plank
+sudo apt install greybird-gtk-theme -y
+sudo apt install plank -y
 plank --preferences 
-sudo apt install conky-all
+wait
+sudo apt install conky-all -y
+wait
+cowsay "Desktop env setup complete!"
+sleep 5
